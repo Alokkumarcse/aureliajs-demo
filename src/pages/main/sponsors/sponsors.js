bbbd515c793ac8b95a1c627c5232b6e62dd97d0f
Alok Kumar <alokk13799@gmail.com>
2024-03-10 23:05:06 +0530

    fix: generate dynamically route using router.generate()

--- src/pages/main/sponsors/sponsors.js
+++ src/pages/main/sponsors/sponsors.js
@@ -1,16 +1,21 @@
 import { inject } from "aurelia-framework";
+import { Router } from "aurelia-router";
 
 import { DataCache } from "../../../services/data-cache";
-@inject(DataCache)
+@inject(DataCache, Router)
 export class Sponsors {
-  constructor(dataCache) {
-    this.sponsors = "Sponsors component";
+  constructor(dataCache, router) {
     this.dataCache = dataCache;
+    this.router = router;
+    this.sponsors = "Sponsors component";
     this.usersData = [];
   }
 
   async activate() {
     this.usersData = await this.dataCache.getUsers();
+    this.usersData.forEach(
+      (item) => (item.detailUrl = this.router.generate("User", { id: item.id }))
+    );
     console.log(this.usersData);
   }
 }
