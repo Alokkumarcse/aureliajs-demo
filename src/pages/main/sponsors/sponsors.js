1a15e9cc9067f227108c3f4bf3ce8ca0173835d9
Alok Kumar <alokk13799@gmail.com>
2024-03-10 19:56:05 +0530

    fix: routing added in nav menu

--- src/pages/main/sponsors/sponsors.js
+++ src/pages/main/sponsors/sponsors.js
@@ -1,5 +1,16 @@
+import { inject } from "aurelia-framework";
+
+import { DataCache } from "../../../services/data-cache";
+@inject(DataCache)
 export class Sponsors {
-  constructor() {
+  constructor(dataCache) {
     this.sponsors = "Sponsors component";
+    this.dataCache = dataCache;
+    this.usersData = [];
+  }
+
+  async activate() {
+    this.usersData = await this.dataCache.getUsers();
+    console.log(this.usersData);
   }
 }
--- src/services/data-cache.js
+++ src/services/data-cache.js
@@ -1,9 +1,31 @@
 import { singleton, transient } from "aurelia-framework";
+import { UsersData } from "./user-info-services";
 
 @singleton()
 export class DataCache {
   constructor() {
     this.data = [];
+    this.userData = UsersData;
     console.log("from dataCache constructor");
+  }
+
+  //making api call to get users data
+  getUsers() {
+    var promise = new Promise((resolve, reject) => {
+      if (!this.userData) {
+        setTimeout(() => {
+          this.userData = UsersData;
+          resolve(this.userData);
+        }, 2000);
+      } else {
+        resolve(this.userData);
+      }
+    });
+    return promise;
+  }
+
+  // making api call to get a single user data
+  getUser(userId) {
+    return this.userData.find((item) => item.id === userId);
   }
 }
