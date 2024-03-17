cc06a6e9b12f5b8171ddd29d1440e6d06502714e
Alok Kumar <alokk13799@gmail.com>
2024-03-17 23:59:34 +0530

    feat: Prevent the loading of jobs route by using canActivate() router life cycle

--- src/pages/main/jobs/jobs.js
+++ src/pages/main/jobs/jobs.js
@@ -3,6 +3,15 @@ export class Jobs {
     this.job = "jobs";
   }
 
+  //canActivate life cycle help to reject to navigation to route
+  canActivate(params, routeConfig, navigationInstruction) {
+    // added an arbitrary time delay to show an spinning cog while loading the routed page
+    let promise = new Promise((resolve, reject) => {
+      setTimeout((_) => resolve(false), 1000);
+    });
+    return promise;
+  }
+
   activate() {
     // added an arbitrary time delay to show an spinning cog while loading the routed page
     let promise = new Promise((resolve, reject) => {
